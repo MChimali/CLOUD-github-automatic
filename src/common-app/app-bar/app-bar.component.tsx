fe61e7d3b3194f176b4492d7000be7e23faa239d
Alpi <chimalimiguel@gmail.com>
2022-03-07 14:39:36 +0100

    change image app bar

--- src/common-app/app-bar/app-bar.component.tsx
+++ src/common-app/app-bar/app-bar.component.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import logo from 'assets/logo.png';
+import logo3 from 'assets/logo.png';
 import * as classes from './app-bar.styles';
 
 export const AppBarComponent: React.FunctionComponent = () => {
   return (
     <AppBar position="static">
       <Toolbar variant="dense" className={classes.root}>
-        <img className={classes.logo} src={logo} />
+        <img className={classes.logo} src={logo3} />
       </Toolbar>
     </AppBar>
   );
